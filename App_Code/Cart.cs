0744f7b589b9b0e68f2259a8a54b604ae3629860
adil shafiq <adilshafiq18@gmail.com>
2013-01-10 23:02:00 +0500

    initial commit

---
+++ App_Code/Cart.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+/// <summary>
+/// TechieTogs shopping cart
+/// </summary>
+public class Cart
+{
+    private List<CartItem> _Items = new List<CartItem>();
+
+    public List<CartItem> Items { get { return _Items; } }
+    public decimal TotalValue { get {
+         decimal sum = 0;
+         foreach(var jojo in _Items)
+         {
+             sum = sum  + jojo.Price;
+         } 
+         return sum;
+         
+    } }
+    public void AddItem(int productID, string name,string description, decimal price)
+    {
+          _Items.Add( new CartItem { ProductID = productID, Name = name,Description = description, Price = price } );
+    }
+    public void Clear()
+    {
+          _Items.Clear();
+    }
+    public void RemoveItem(int index)   
+    {
+          _Items.RemoveAt(index);
+    }
+}
+
+public class CartItem
+{
+    public int ProductID { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public decimal Price { get; set; }
+}
